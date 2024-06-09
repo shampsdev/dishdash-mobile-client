7a119df3094b0142619280075ecf9a4dfe68a2f1
Mike de Geofroy <59496187+mikedegeofroy@users.noreply.github.com>
2024-06-09 23:03:01 +0300

    added views around styled text

--- src/entities/swiped-card/swipe-card.tsx
+++ src/entities/swiped-card/swipe-card.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { View, Image, StyleSheet, Text, Pressable, ViewStyle, ImageProps, ImageBackground } from 'react-native'
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
-
+import React from 'react';
 import {
-  Gesture,
-  GestureDetector
-} from 'react-native-gesture-handler';
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  Pressable,
+  ViewStyle,
+  ImageProps,
+  ImageBackground,
+} from 'react-native';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import { ICard } from './card.interface';
 
-const categories = ['Кофе', 'Развлечения', 'Чай', 'Новые ощущения']
+const categories = ['Кофе', 'Развлечения', 'Чай', 'Новые ощущения'];
 
 export interface SwipeCardProps {
   card: ICard;
@@ -36,7 +47,7 @@ export const SwipeCard = (props: SwipeCardProps) => {
         runOnJS(props.onSwipe)(props.card.id);
       } else if (valueX > VALUE_FOR_SWIPING) {
         rotate.value = withSpring(10);
-        offsetX.value = withSpring(300)
+        offsetX.value = withSpring(300);
         runOnJS(props.onSwipe)(props.card.id);
       } else {
         offsetX.value = withSpring(0);
@@ -44,14 +55,13 @@ export const SwipeCard = (props: SwipeCardProps) => {
       }
     });
 
-
   const animatedStyles = useAnimatedStyle(() => ({
     transform: [
       { translateX: offsetX.value },
       { rotate: `${rotate.value}deg` },
     ],
     // marginBottom: -props.index * 10,
-    zIndex: -props.index
+    zIndex: -props.index,
   }));
 
   const shadowStyle = props.index === 0 ? styles.activeCardShadow : {};
@@ -59,12 +69,18 @@ export const SwipeCard = (props: SwipeCardProps) => {
   return (
     <View style={styles.container}>
       <GestureDetector gesture={pan}>
-        <Animated.View style={[animatedStyles, {
-          width: '80%',
-          height: '100%'
-        }, shadowStyle]}>
+        <Animated.View
+          style={[
+            animatedStyles,
+            {
+              width: '80%',
+              height: '100%',
+            },
+            shadowStyle,
+          ]}
+        >
           <View className='relative h-3/4'>
-            <ImageBackground 
+            <ImageBackground
               className='h-full'
               imageStyle={{
                 borderRadius: 24,
@@ -72,7 +88,8 @@ export const SwipeCard = (props: SwipeCardProps) => {
               source={{ uri: props.card.image }}
             />
 
-            <View style={{
+            <View
+              style={{
                 position: 'absolute',
                 display: 'flex',
                 flexDirection: 'row',
@@ -81,59 +98,63 @@ export const SwipeCard = (props: SwipeCardProps) => {
                 width: '100%',
                 paddingHorizontal: 12,
                 top: 10,
-              }} >
-              <Text
+              }}
+            >
+              <View
                 style={{
                   backgroundColor: 'rgba(255, 255, 255, 0.9)',
                   paddingHorizontal: 10,
                   paddingVertical: 6,
                   borderRadius: 999,
                 }}
-                className='text-lg rounded-3xl'
               >
-                { props.card.title }
-              </Text>
+                <Text className='text-lg rounded-3xl'>{props.card.title}</Text>
+              </View>
 
-            <Pressable style={{
-              backgroundColor: 'rgba(255, 255, 255, 0.9)',
-              borderRadius: 999,
-              width: 40,
-              height: 40
-            }}/>
+              <Pressable
+                style={{
+                  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                  borderRadius: 999,
+                  width: 40,
+                  height: 40,
+                }}
+              />
             </View>
           </View>
-          <View style={{
-            shadowColor: 'black',
-            elevation: 8,
-            shadowRadius: 4,
-            shadowOffset: { width: 10, height: 5 },
-            shadowOpacity: 0.5
-          }} className='w-full py-2 h-44 bg-white -translate-y-9 rounded-3xl items-center'>
+          <View
+            style={{
+              shadowColor: 'gray',
+              elevation: 8,
+              shadowRadius: 4,
+              shadowOffset: { width: 10, height: 5 },
+              shadowOpacity: 0.5,
+            }}
+            className='w-full py-2 h-44 bg-white -translate-y-9 rounded-3xl items-center'
+          >
             <View className='flex-row flex-wrap max-w-[96%] gap-1'>
-            {
-              categories.map((category, index) => 
-              <Text 
-                className='bg-gray-300 px-4 py-2 rounded-full' 
-                key={index}
-              >{ category }</Text>)
-            }
+              {categories.map((category, index) => (
+                <View
+                  className='bg-gray-300 px-4 py-2 rounded-full'
+                  key={index}
+                >
+                  <Text>{category}</Text>
+                </View>
+              ))}
             </View>
 
-            <Text className='p-3'>
-              { props.card.shortDescription }
-            </Text>
+            <Text className='p-3'>{props.card.shortDescription}</Text>
           </View>
         </Animated.View>
       </GestureDetector>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   activeCardShadow: {
     shadowColor: 'black',
