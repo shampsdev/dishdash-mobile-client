3049a2caf54e328664c7c20c854a05441f811287
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-07-15 00:24:58 +0200

    removed env params

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,6 +1,7 @@
 import { ImageSourcePropType } from 'react-native';
 
-export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+// export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+export const API_URL = "https://dishdash.ru/";
 
 export interface ImageFile {
   src: ImageSourcePropType;
