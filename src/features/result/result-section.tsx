18d143f4d355e1ce11aaeb2933e5efd71d1f375f
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-07-14 22:28:48 +0200

    Added a map to result, made it a little better looking

--- src/features/result/result-section.tsx
+++ src/features/result/result-section.tsx
@@ -10,6 +10,7 @@ import * as Location from 'expo-location';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '@/app/navigation.interface';
+import MapView from 'react-native-maps';
 
 const openGoogleMaps = async (
   destinationLatitude: number,
@@ -57,30 +58,50 @@ export const ResultSection = () => {
   return card ? (
     <View
       style={{
-        rowGap: 16, 
+        rowGap: 16,
       }}
-      className='h-full w-[85%] mx-auto relative items-center'
+      className='h-full relative items-center'
     >
       <ScrollView className='h-min' style={styles.textContainer}>
-        <View>
+        <View className='w-[85%] mx-auto' style={styles.shadow}>
           <Image
             className='w-full min-h-[240px] h-[33vh] rounded-2xl'
             src={card.Image}
           />
-          <View className='w-full p-4 bg-white rounded-2xl'>
-            <CustomText className='font-semibold text-2xl'>
+          <View className='w-full p-4 bg-white -translate-y-10 rounded-2xl'>
+            <CustomText className='font-semibold text-xl pb-2'>
               {card.Title}
             </CustomText>
             <View className='flex-row space-x-1'>
               <MapPointIcon width={24} height={24} />
-              <CustomText className='text-[#7F7F7F] text-xl'>
+              <CustomText className='text-[#7F7F7F] text-lg'>
                 {card.Address}
               </CustomText>
             </View>
           </View>
         </View>
-        <CustomText className='text-xl pb-2'>Коротко о месте</CustomText>
-        <CustomText>{card.Description}</CustomText>
+        <View className='w-[85%] mx-auto pb-6'>
+          <CustomText className='text-xl pb-2'>Коротко о месте</CustomText>
+          <CustomText>{card.Description}</CustomText>
+        </View>
+        <MapView
+          onTouchStart={async () =>
+            await openGoogleMaps(card.Location.lat, card.Location.lon)
+          }
+          scrollEnabled={false}
+          initialRegion={{
+            latitude: card.Location.lat,
+            longitude: card.Location.lon,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001,
+          }}
+          style={{
+            height: '30%',
+            width: '85%',
+            marginHorizontal: 'auto',
+            borderRadius: 14,
+          }}
+        />
       </ScrollView>
       <View
         style={{
@@ -97,14 +118,6 @@ export const ResultSection = () => {
         >
           Выйти
         </CustomButton>
-        <CustomButton
-          onPress={async () =>
-            await openGoogleMaps(card.Location.lat, card.Location.lon)
-          }
-          type='primary'
-        >
-          На карте
-        </CustomButton>
       </View>
     </View>
   ) : (
@@ -114,13 +127,16 @@ export const ResultSection = () => {
 
 const styles = StyleSheet.create({
   textContainer: {
+    width: '100%',
+    borderRadius: 18,
+    paddingTop: 12,
+    paddingBottom: 24,
+  },
+  shadow: {
     shadowColor: 'black',
     elevation: 8,
     shadowRadius: 10,
     shadowOffset: { width: 10, height: 5 },
     shadowOpacity: 0.2,
-    width: '100%',
-    borderRadius: 18,
-    paddingVertical: 12,
   },
 });
